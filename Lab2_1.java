e8b1e9d3a5c922c27b5d4d0b435da66fdd31dc33
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-10-12 12:11:00 +0300

    Create Lab2_1.java

---
+++ Lab2_1.java
@@ -0,0 +1,29 @@
+package com.company;
+import mpi.*;
+
+public class Lab2_1 {
+    public static void main(String[] args) {
+        MPI.Init(args);
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        int[] buf = {rank};
+        int[] s = new int[1];
+        int TAG = 0;
+        if (rank == 0)
+        {
+            MPI.COMM_WORLD.Send(buf, 0, 1, MPI.INT, (rank + 1) % size, TAG);
+            MPI.COMM_WORLD.Recv(s, 0, 1, MPI.INT, (rank - 1 + size) % size, TAG);
+            System.out.println("Processor " + rank + " has finished with sum: " + s[0]);
+        }
+        else
+        {
+            MPI.COMM_WORLD.Recv(s, 0, 1, MPI.INT, (rank - 1 + size) % size, TAG);
+            buf[0] += s[0];
+            MPI.COMM_WORLD.Send(buf, 0, 1, MPI.INT, (rank + 1) % size, TAG);
+
+        }
+        MPI.Finalize();
+    }
+}
+
+
