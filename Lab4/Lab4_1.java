42ecce19f40a0cafc65c35c08f207c34765fe42d
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-11-16 18:24:35 +0300

    Update Lab4_1.java

--- Lab4/Lab4_1.java
+++ Lab4/Lab4_1.java
@@ -4,12 +4,14 @@ import mpi.*;
 public class Lab4_1 {
 
     private static void initMatrix(int[][] matrix) {
+        // Инициализация матрицы случайными значениями
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[0].length; j++)
                 matrix[i][j] = (int) (Math.random() * 10);
     }
 
     private static void initVector(int[] vector) {
+        // Инициализация вектора случайными значениями
         for (int i = 0; i < vector.length; i++)
             vector[i] = (int) (Math.random() * 10);
     }
@@ -35,6 +37,12 @@ public class Lab4_1 {
         return localResult;
     }
 
+    static void printVector(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " ");
+        }
+    }
+
     private static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -44,73 +52,38 @@ public class Lab4_1 {
         }
     }
 
-    private static void printVector(int[] vector) {
-        for (int i = 0; i < vector.length; i++) {
-            System.out.print(vector[i] + " ");
-        }
-    }
-
-    private static int[] matrixToArray(int[][] array) {
-        int rows = array.length;
-        int cols = array[0].length;
-        int[] flattened = new int[rows * cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                flattened[i * cols + j] = array[i][j];
-            }
-        }
-        return flattened;
-    }
-
-    private static int[][] matrixFrom1DArray(int[] array, int rows, int cols) {
-        int[][] matrix = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] = array[i * cols + j];
-            }
-        }
-        return matrix;
-    }
-
     public static void main(String[] args) throws MPIException {
         MPI.Init(args);
-
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-
         int n = 1000;
         int[][] matrix = new int[n][n];
         int[] vector = new int[n];
-        int[] localMatrix;
-        int[] localVector;
-        int[] partialResult;
+        int[] result;
 
         if (rank == 0) {
-            initMatrix(matrix);
-            initVector(vector);
+          initMatrix(matrix);
+          initVector(vector);
         }
+        int[][] localMatrix = new int[n][n];
+        int[] localVector = new int[n];
 
-        localMatrix = new int[n * n / size + 1];
-        localVector = new int[n / size];
+        MPI.COMM_WORLD.Scatter(matrix, 0, n , MPI.OBJECT, localMatrix, 0, n, MPI.OBJECT, 0);
+        MPI.COMM_WORLD.Scatter(vector, 0, n, MPI.INT, localVector, 0, n, MPI.INT, 0);
 
-        long startTime = System.nanoTime();
+        result = multiplyMatrixVector(localMatrix, localVector);
 
-        MPI.COMM_WORLD.Scatter(matrixToArray(matrix), 0, n * n / size, MPI.INT, localMatrix, 0, n * n / size, MPI.INT, 0);
-        MPI.COMM_WORLD.Scatter(vector, 0, n / size, MPI.INT, localVector, 0, n / size, MPI.INT, 0);
-
-        partialResult = multiplyMatrixVector(matrixFrom1DArray(localMatrix, n / size, n), localVector);
-
-        int[] globalResult = new int[n];
-        MPI.COMM_WORLD.Gather(partialResult, 0, n / size, MPI.INT, globalResult, 0, n / size, MPI.INT, 0);
-
-        long endTime = System.nanoTime();
-        long executionTime = (endTime - startTime) / 1_000_000;
+        int[] finalResult = new int[n];
+        MPI.COMM_WORLD.Gather(result, 0, n , MPI.INT, finalResult, 0, n , MPI.INT, 0);
 
         if (rank == 0) {
-            System.out.println("Result vector:");
-            printVector(globalResult);
-            System.out.println("\nExecution Time: " + executionTime + " milliseconds");
+            System.out.print("Matrix-Vector Multiplication Result: ");
+            for (int i = 0; i < n; i++) {
+                System.out.print(finalResult[i] + " ");
+            }
+            System.out.println();
         }
+
         MPI.Finalize();
     }
 }
